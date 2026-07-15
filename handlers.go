6a2d39e27401abe612d751d7c28b30c036ae364c
PokeMasterCP <christian.adrn.perez@gmail.com>
2026-07-15 11:19:25 -0500

    add user creation endpoint and database support using sqlc

--- handlers.go
+++ handlers.go
@@ -27,17 +27,22 @@ func (cfg *apiConfig) handlerNumHits(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(msg))
 }
 
-func (cfg *apiConfig) handlerResetHits(w http.ResponseWriter, req *http.Request) {
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	if cfg.platform != "dev" {
+		helperJSONError(w, "Forbidden outside of dev", http.StatusForbidden)
+		return
+	}
 	cfg.fileServerHits.Store(0)
+	log.Println("reset file server hits")
+	cfg.db.DeleteAllUsers(req.Context())
+	log.Println("deleted all users")
+
+	helperRespondWithJSON(struct{}{}, w, http.StatusOK)
 }
 
 func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
 	type chirp struct {
 		Body string `json:"body"`
-	}
-
-	type errorResponse struct {
-		Error string `json:"error"`
 	}
 
 	type validResponse struct {
@@ -48,20 +53,50 @@ func handlerValidateChirp(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		errorMessage := fmt.Sprintf("failed to unmarshal json request: %s", err)
 		log.Println(errorMessage)
-		response := errorResponse{Error: errorMessage}
-		helperRespondWithJSON(response, w, http.StatusInternalServerError)
+		helperJSONError(w, errorMessage, http.StatusInternalServerError)
 		return
 	}
 
 	if len(params.Body) > 140 {
-		response := errorResponse{Error: "Chirp is too long"}
-		helperRespondWithJSON(response, w, http.StatusBadRequest)
+		helperJSONError(w, "Chirp is too long", http.StatusBadRequest)
 		return
 	}
 
 	cleanedChirp := helperCleanOutput(params.Body)
 	response := validResponse{CleanedBody: cleanedChirp}
 	helperRespondWithJSON(response, w, http.StatusOK)
+}
+
+func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	params, err := helperReadJSON[parameters](req.Body)
+	if err != nil {
+		errorMessage := fmt.Sprintf("failed to unmarshal json request: %s", err)
+		log.Println(errorMessage)
+		helperJSONError(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+
+	newUser, err := cfg.db.CreateUser(req.Context(), params.Email)
+	if err != nil {
+		errorMessage := fmt.Sprintf("error creating user: %s", err)
+		log.Println(errorMessage)
+		helperJSONError(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+	log.Printf("created user: %s", newUser)
+
+	user := User{
+		ID:        newUser.ID,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+		Email:     newUser.Email,
+	}
+
+	helperRespondWithJSON(user, w, http.StatusCreated)
 }
 
 func helperReadJSON[T any](req io.Reader) (T, error) {
@@ -73,6 +108,24 @@ func helperReadJSON[T any](req io.Reader) (T, error) {
 		return params, err
 	}
 	return params, nil
+}
+
+func helperJSONError(w http.ResponseWriter, msg string, statusCode int) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	payload := errorResponse{Error: msg}
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("failed to marshal response: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(data)
 }
 
 func helperRespondWithJSON[T any](payload T, w http.ResponseWriter, statusCode int) {
--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+	"time"
+
+	"github.com/PokeMasterCP/chirp/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
+	db             *database.Queries
+	platform       string
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %s", err)
+	}
+
 	PORT := "8080"
 	mux := http.NewServeMux()
 	cfg := apiConfig{}
+	cfg.db = database.New(db)
+	cfg.platform = os.Getenv("PLATFORM")
 
 	mux.HandleFunc("GET /api/healthz", handlerHealth)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerNumHits)
-	mux.HandleFunc("POST /admin/reset", cfg.handlerResetHits)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", cfg.handlerCreateUser)
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 
 	s := &http.Server{
